0bc530a287a4059d6ae60b6ff1b1a2b32ceb13c8
Ryan Luu <ryan.luu@ryanluu.dev>
2025-11-19 00:21:36 -0800

    Add shortcut control

--- Global Shortcut/MainWindow.xaml.cs
+++ Global Shortcut/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using DevWinUI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,6 +6,12 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Intrinsics.X86;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -26,6 +28,23 @@ namespace Global_Shortcut
         public MainWindow()
         {
             InitializeComponent();
+            MainShortcut.Keys = ["F6"];
+        }
+
+        private void OnMainShortcutPrimaryButtonClick(object sender, ContentDialogButtonClickEventArgs e)
+        {
+            MainShortcut.UpdatePreviewKeys();
+            MainShortcut.CloseContentDialog();
+        }
+
+        private void OnMainShortcutSecondaryButtonClick(object sender, ContentDialogButtonClickEventArgs e)
+        {
+            // "Secondary button clicked!";
+        }
+
+        private void OnMainShortcutCloseButtonClick(object sender, ContentDialogButtonClickEventArgs e)
+        {
+            // "Close button clicked!";
         }
     }
 }
